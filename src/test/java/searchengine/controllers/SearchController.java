3485e09e9c0dbc232934545af7488b4d8840530c
workjava9 <pasakirpicev6377@yandex.ru>
2025-08-06 14:51:24 +0300

    docs(readme): обновлено описание проекта, инструкция по тестам и зависимостям морфологии

--- src/test/java/searchengine/controllers/SearchController.java
+++ src/test/java/searchengine/controllers/SearchController.java
@@ -1,30 +1,30 @@
-package searchengine.controllers;
-
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-import searchengine.dto.Response;
-import searchengine.services.search.SearchService;
-
-@RestController
-@RequestMapping("/api")
-public class SearchController {
-
-    private final SearchService searchService;
-
-    public SearchController(SearchService searchService) {
-        this.searchService = searchService;
-    }
-
-    @GetMapping("/search")
-    public ResponseEntity<Response> search(
-            @RequestParam String query,
-            @RequestParam(required = false) String site,
-            @RequestParam(defaultValue = "0") int offset,
-            @RequestParam(defaultValue = "10") int limit) {
-        Response response = searchService.search(query, site, offset, limit);
-        return ResponseEntity.ok(response);
-    }
-}
+//package searchengine.controllers;
+//
+//import org.springframework.http.ResponseEntity;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RequestParam;
+//import org.springframework.web.bind.annotation.RestController;
+//import searchengine.dto.Response;
+//import searchengine.services.search.SearchService;
+//
+//@RestController
+//@RequestMapping("/api")
+//class SearchController {
+//
+//    private final SearchService searchService;
+//
+//    public SearchController(SearchService searchService) {
+//        this.searchService = searchService;
+//    }
+//
+//    @GetMapping("/search")
+//    public ResponseEntity<Response> search(
+//            @RequestParam String query,
+//            @RequestParam(required = false) String site,
+//            @RequestParam(defaultValue = "0") int offset,
+//            @RequestParam(defaultValue = "10") int limit) {
+//        Response response = searchService.search(query, site, offset, limit);
+//        return ResponseEntity.ok(response);
+//    }
+//}
